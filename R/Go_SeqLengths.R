599655cad7959087eaed359b1c5cda8904bbcf60
bbagy <bbagy81@gmail.com>
2024-02-20 11:06:46 -0500

    Gotools_1.0.4

--- R/Go_SeqLengths.R
+++ R/Go_SeqLengths.R
@@ -21,31 +21,31 @@
 #' @importFrom phyloseq otu_table sample_data merge_phyloseq
 #' @importFrom stats nchar table
 #' @importFrom graphics hist
-Go_SeqLengths <- function(psIN, 
+Go_SeqLengths <- function(psIN,
                           filter=NULL){
-  
+
   # Extract ASV sequences from the phyloseq object
   seqtab <- otu_table(psIN)
-  
+
   asv_sequences <- colnames(seqtab)
-  
+
   if(all(strsplit(asv_sequences, "")[[1]] %in% c("A", "T", "C", "G", "N"))) {
   } else {
     seqtab <- t(otu_table(psIN))
     asv_sequences <- colnames(seqtab)
   }
-  
-  
-  
-  
+
+
+
+
   if(is.null(filter)){
     # Calculate the length of each sequence
     sequence_lengths <- nchar(asv_sequences)
     # Create a table of sequence length distribution
     length_distribution <- table(sequence_lengths)
-    
+
     # table(nchar(getSequences(seqtab)))
-    
+
     # Print the distribution
     print(length_distribution)
     # Plot a histogram of sequence lengths
@@ -57,20 +57,20 @@ Go_SeqLengths <- function(psIN,
     # Create a table of sequence length distribution
     length_distribution <- table(sequence_lengths)
     hist(sequence_lengths, breaks=100, main="Distribution of Sequence Lengths (filtered)", xlab="Sequence Length", ylab="Frequency")
-    
-    
-    new_ps1 <- phyloseq(seqtab2, tax_table(ps1))
-    
-    if (!is.null(sample_data(ps1))) {
-      new_ps1 <- merge_phyloseq(new_ps1, sample_data(ps1))
+
+
+    new_ps1 <- phyloseq(seqtab2, tax_table(psIN))
+
+    if (!is.null(sample_data(psIN))) {
+      new_ps1 <- merge_phyloseq(new_ps1, sample_data(psIN))
     }
-    
-    if (!is.null(phy_tree(ps1))) {
-      new_ps1 <- merge_phyloseq(new_ps1, phy_tree(ps1))
+
+    if (!is.null(phy_tree(psIN))) {
+      new_ps1 <- merge_phyloseq(new_ps1, phy_tree(psIN))
     }
-    
+
   return(new_ps1)
   }
 
-  
+
 }
