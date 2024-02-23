64fb7494f98552d7a5d4801ddc59d29da91f0740
bbagy <bbagy81@gmail.com>
2024-02-23 15:08:21 -0500

    Gotools_1.0.4

--- R/Go_function2ps_V2.R
+++ R/Go_function2ps_V2.R
@@ -24,83 +24,63 @@
 #'
 #' @export
 
+version2;
+Go_function2ps <- function(tabPath, project = NULL, func.type, name = NULL) {
+  # Read tab
+  func.tab <- try(read_tsv(tabPath, col_types = cols()), silent = TRUE)
 
-Go_function2ps <- function(tabPath,
-                           project=NULL, func.type,
-                           name=NULL){
-  
-  tt<- try(class(func.type),T)
-  
-  if(class(tt) == "try-error"){
-    print("Please define the data type. PICRUSt or Hummann")
-    break
-  }else if(any(grepl(func.type, c("picrust","Picrust","Picrust2","PICRUSt","PICRUSTt2")))){
+  if (inherits(func.tab, "try-error")) {
+    print("Error reading the table. Check the file path and format.")
+    return(NULL)
+  }
+
+  # Identify data type and perform necessary preprocessing
+  if (any(grepl(func.type, c("picrust", "Picrust", "Picrust2", "PICRUSt", "PICRUSTt2")))) {
     func.type <- "PICRUSTt2"
-    
-    # Read tab
-    func.tab <- read_tsv(tabPath,col_types = cols())
-    
-  }else if(any(grepl(func.type, c("Human","Humann","humann","human","Human2","Humann2","humann2","human2")))){
+  } else if (any(grepl(func.type, c("Human", "Humann", "humann", "human", "Human2", "Humann2", "humann2", "human2")))) {
     func.type <- "Humann2"
-    
-    func.tab <- read.csv(sprintf("%s",tabPath), header=T, as.is=T, sep="\t", row.names=1, comment.char="", quote="")
-    
-    for (rev in c("NO_NAME", "ribosomal protein","UNGROUPED","unclassified","UNMAPPED","UNINTEGRATED")){
-      func.tab <- subset(func.tab, !(grepl(rev, rownames(func.tab))))
-      print(dim(func.tab))
-    }
-    
-    
-    rownames(func.tab) <- gsub(",", ".", rownames(func.tab));head(rownames(func.tab))
-    func.tab1 <- data.frame(str_split(rownames(func.tab), ": ", simplify = T));head(func.tab)
-    func.tab$X3 <- NULL; head(func.tab)
-    
-    
+    # Additional preprocessing specific to Humann2
+    func.tab <- func.tab[!grepl("ribosomal protein|UNGROUPED|unclassified|UNMAPPED|UNINTEGRATED", rownames(func.tab)), ]
+    rownames(func.tab) <- gsub(",", ".", rownames(func.tab))
+    func.tab1 <- data.frame(str_split(rownames(func.tab), ": ", simplify = T))
+    func.tab <- cbind(description = func.tab1$X2, func.tab)
     rownames(func.tab) <- func.tab1$X1
-    
-    func.tab <- cbind(description =func.tab1$X2, func.tab)
-    
+  } else {
+    print("Please define the data type: PICRUSt or Humann")
+    return(NULL)
   }
-  
-  NumOFsample <- dim(func.tab)[2]
-  
-  # split a data frame
-  otu <- as.matrix(func.tab[,3:NumOFsample])
-  tax <- as.matrix(func.tab1[,1:2])
-  
-  tax.df <- data.frame(tax)
-  rownames(tax.df) <- tax.df$X1
-  tax <- as.matrix(tax.df)
-  
-  if (func.type == "PICRUSTt2"){
-    rownames(otu) <- tax[,1];dim(otu)
-    rownames(tax) <- tax[,1];dim(tax)
-  }
-  
-  
-  
-  # define kegg or pathway
-  if (any(grepl("K0", rownames(tax)))){
-    colnames(tax) <- c("KO","KO.des")
+
+  # Define number of samples and split data frame
+  NumOfSample <- dim(func.tab)[2]
+  otu <- as.matrix(func.tab[, 3:NumOfSample])
+  tax <- as.matrix(func.tab[, 1:2])
+  rownames(otu) <- tax[, 1]
+  rownames(tax) <- tax[, 1]
+
+  # Define KEGG or pathway
+  if (any(grepl("K0", rownames(tax)))) {
+    colnames(tax) <- c("KO", "KO.des")
     func <- "KEGG"
-  }else if (any(grepl("PWY", rownames(tax)))){
-    colnames(tax) <- c("pathway","path.des")
+  } else if (any(grepl("PWY", rownames(tax)))) {
+    colnames(tax) <- c("pathway", "path.des")
     func <- "pathway"
+  } else {
+    func <- NULL
   }
-  
-  #merge phyloseq
-  ps <- phyloseq(otu_table(otu, taxa_are_rows=T), tax_table(tax));ps
-  
+
+  # Merge phyloseq
+  ps <- phyloseq(otu_table(otu, taxa_are_rows = TRUE), tax_table(tax))
+
   print(ps)
-  
-  
-  # saving file
-  rds <- file.path("2_rds") 
-  if(!file_test("-d", rds)) dir.create(rds)
-  saveRDS(ps, sprintf("%s/ps.%s.%s.%s.%s%s.rds",rds,
+
+  # Saving file
+  rds <- file.path("2_rds")
+  if (!file_test("-d", rds)) dir.create(rds)
+  saveRDS(ps, sprintf("%s/ps.%s.%s.%s%s%s.rds", rds,
                       func.type,
-                      func,
-                      project,
-                      ifelse(is.null(name), "", paste(name, ".", sep = "")), 
+                      ifelse(is.null(func), "", paste(func, ".", sep = "")),
+                      ifelse(is.null(project), "", paste(project, ".", sep = "")),
+                      ifelse(is.null(name), "", paste(name, ".", sep = "")),
                       format(Sys.Date(), "%y%m%d")))
 }
+
--- R/Go_pheatmap_V2.R
+++ R/Go_pheatmap_V2.R
@@ -53,28 +53,28 @@
 
 Go_pheatmap <- function(psIN,project, title,
                         group1=NULL, group2=NULL, group3=NULL, group4=NULL,
-                        Ntax=NULL, 
-                        name=NULL, 
+                        Ntax=NULL,
+                        name=NULL,
                         col_orders=NULL,
                         show_rownames = T,show_colnames = F,
                         cutree_rows = NA, cutree_cols = NA,
-                        cluster_rows = T, cluster_cols = T, 
+                        cluster_rows = T, cluster_cols = T,
                         showPhylum = T,
                         width){
   # BiocManager::install("ComplexHeatmap")
   # install.packages("Cairo")
   if(!is.null(dev.list())) dev.off()
-  
+
   # out dir
-  out <- file.path(sprintf("%s_%s",project, format(Sys.Date(), "%y%m%d"))) 
+  out <- file.path(sprintf("%s_%s",project, format(Sys.Date(), "%y%m%d")))
   if(!file_test("-d", out)) dir.create(out)
-  out_pdf <- file.path(sprintf("%s_%s/pdf",project, format(Sys.Date(), "%y%m%d"))) 
+  out_pdf <- file.path(sprintf("%s_%s/pdf",project, format(Sys.Date(), "%y%m%d")))
   if(!file_test("-d", out_pdf)) dir.create(out_pdf)
-  out_tab <- file.path(sprintf("%s_%s/table",project, format(Sys.Date(), "%y%m%d"))) 
+  out_tab <- file.path(sprintf("%s_%s/table",project, format(Sys.Date(), "%y%m%d")))
   if(!file_test("-d", out_tab)) dir.create(out_tab)
-  out_pheatmapTab <- file.path(sprintf("%s_%s/table/pheatmapTab",project, format(Sys.Date(), "%y%m%d"))) 
+  out_pheatmapTab <- file.path(sprintf("%s_%s/table/pheatmapTab",project, format(Sys.Date(), "%y%m%d")))
   if(!file_test("-d", out_pheatmapTab)) dir.create(out_pheatmapTab)
-  
+
 
   #----- normalization relative abundant ---#
    if(max(data.frame(otu_table(psIN))) < 1){
@@ -83,22 +83,22 @@ Go_pheatmap <- function(psIN,project, title,
    }else{
      print("Counts or cpm table data. Data normalized by perentage.")
      psIN.prune = prune_samples(sample_sums(psIN) > 1, psIN);psIN.prune
-     ps.rel <- transform_sample_counts(psIN.prune, function(x) log(1+x) );ps.rel# x/sum(x)*100 
+     ps.rel <- transform_sample_counts(psIN.prune, function(x) log(1+x) );ps.rel# x/sum(x)*100
    }
-   
+
 print("Check the psIN")
-   
+
   if(is.null(Ntax)){
     Ntax <- dim(tax_table(ps.rel))[1]
     print(sprintf("number of taxa is %s",Ntax))
     ps.rel.sel <- prune_taxa(names(sort(taxa_sums(ps.rel),TRUE)[1:Ntax]), ps.rel);ps.rel.sel
   }else{
-    ps.rel.sel <- prune_taxa(names(sort(taxa_sums(ps.rel),TRUE)[1:Ntax]), ps.rel);ps.rel.sel 
+    ps.rel.sel <- prune_taxa(names(sort(taxa_sums(ps.rel),TRUE)[1:Ntax]), ps.rel);ps.rel.sel
   }
 
-   
+
   # for height
-   
+
    if ( dim(tax_table(ps.rel.sel))[1] > 30){
      h=8
    }else if(dim(tax_table(ps.rel.sel))[1] <= 30 & dim(tax_table(ps.rel.sel))[1] > 25){
@@ -112,26 +112,26 @@ print("Check the psIN")
    }else if(dim(tax_table(ps.rel.sel))[1] <= 10){
      h=4.5
    }
-   
-  
-  matrix <- data.frame(t(otu_table(ps.rel.sel)))
-  
 
-  
+
+  matrix <- data.frame(t(otu_table(ps.rel.sel)))
+
+
+
   # normalization for log2
   is.na(matrix)<-sapply(matrix, is.infinite)
   matrix[is.na(matrix)]<-0
-  
+
   matrix <- matrix[,colSums(is.na(matrix))<nrow(matrix)]
   colnames(matrix) <- gsub("X","",colnames(matrix))
-  
-  
 
-  
+
+
+
  # get data tyep
   print("Check the data type")
   taxtab.col <- colnames(data.frame((tax_table(ps.rel.sel))))
-  
+
   if (any(grepl("Species", taxtab.col))){
     taxaTab <- data.frame(tax_table(ps.rel.sel)[,"Species"])
     type <- "taxonomy"
@@ -149,27 +149,27 @@ print("Check the psIN")
     type <- "RNAseq"
     print(type)
   }
-  
-  
-  
-  
+
+
+
+
   colnames(taxaTab) <- "Rank"
   taxaTab.print <- data.frame(tax_table(ps.rel.sel))
-  
+
   write.csv(taxaTab.print,file=sprintf("%s/pheatmap.%s.tap(%s).%s%s%s.csv",out_pheatmapTab,
                                        project,
-                                       Ntax, 
-                                       ifelse(is.null(title), "", paste(title, ".", sep = "")), 
-                                       ifelse(is.null(name), "", paste(name, ".", sep = "")), 
+                                       Ntax,
+                                       ifelse(is.null(title), "", paste(title, ".", sep = "")),
+                                       ifelse(is.null(name), "", paste(name, ".", sep = "")),
                                        format(Sys.Date(), "%y%m%d"),
                                        sep="/"), quote = FALSE, col.names = NA)
 
 
   # map 정리
   mapping <- data.frame(sample_data(ps.rel.sel));dim(mapping)
-  
+
   tt <- try(sel <- intersect(rownames(mapping), colnames(matrix)), T)
-  
+
   if (length(tt) == 0){
     sel <- intersect(rownames(mapping), rownames(matrix)); head(sel, 3)
   }else{
@@ -177,54 +177,59 @@ print("Check the psIN")
   }
 
   mapping.sel <- mapping[sel,, drop=F];dim(mapping.sel)
-  
-  
-  
+
+
+
   head(rownames(mapping))
   head(rownames(matrix))
-  
-  
+
+
   # phylum annotation
   # Define a function to assign colors to Phylum/Path
   phylumcolor <- c("#E7695DFF", "#6B8993FF", "#F6F0D4FF", "#95CE8AFF", "#D2D2D2FF", "#94D4D4FF", "#969696FF", "#F1F3E8FF", "#88775FFF")
-  
+
   assign_colors <- function(x, colors) {
     getPalette = colorRampPalette(colors)
     col <- getPalette(length(unique(x)))
     names(col) = levels(x)
     return(col)
   }
-  
+
   # Based on the type, generate annotation row and assign colors
   if(type %in% c("taxonomy", "taxanomy")) {
     annotation_row <- data.frame(Phylum = as.factor(tax_table(ps.rel.sel)[, "Phylum"]))
     phylum_col <- assign_colors(annotation_row$Phylum, phylumcolor)
   } else if(type %in% c("kegg", "pathway")) {
-    annotation_row <- data.frame(Path = as.factor(tax_table(ps.rel.sel)[, "KO" | "pathway"]))
+    if("KO" %in% colnames(tax_table(ps.rel.sel))){
+      annotation_row <- data.frame(Path = as.factor(tax_table(ps.rel.sel)[, "KO"]))
+    } else if("pathway" %in% colnames(tax_table(ps.rel.sel))){
+      annotation_row <- data.frame(Path = as.factor(tax_table(ps.rel.sel)[, "pathway"]))
+    } else {
+      stop("Neither 'KO' nor 'pathway' columns are present in the taxonomy table.")
+    }
+
     Path_col <- assign_colors(annotation_row$Path, phylumcolor)  # Ensure Pathcolor is defined somewhere above
   }else if(type %in% c("RNAseq")) {
     annotation_row <- data.frame(Path = as.factor(tax_table(ps.rel.sel)[, "symbol"]))
     Path_col <- assign_colors(annotation_row$Path, phylumcolor)  # Ensure Pathcolor is defined somewhere above
   }
-  
-  
-  
+
 
 
   tt <- try(rownames(annotation_row) <- rownames(matrix), T)
-  
+
   if (class(tt) == "try-error"){
     rownames(annotation_row) <- colnames(matrix)
   }else{
     rownames(annotation_row) <- rownames(matrix)
   }
-  
-  
-  
+
+
+
   # phylum colors
   # phylum_col = RColorBrewer::brewer.pal(length(levels(annotation_row$Phylum)), "Dark2")
 
-  
+
   # add group(s) and color list
   # Function to generate colors
   generate_colors <- function(group, hardcode_colors=NULL) {
@@ -236,56 +241,56 @@ print("Check the psIN")
     names(color) <- levels(as.factor(unique_vals))
     return(color)
   }
-  
+
   # Function to create annotation_col data frame
   generate_annotation_col <- function(groups) {
     annotation_col <- as.data.frame(lapply(groups, function(x) as.factor(mapping.sel[,x])))
     colnames(annotation_col) <- groups
     return(annotation_col)
   }
-  
+
   # Larger list of hardcoded colors
   all_hardcoded_colors <- list(
     color_set_1 = c("#EB5291FF", "#6351A0FF","#FEF79EFF", "#FBBB68FF", "#F5BACFFF", "#9DDAF5FF","#ECF1F4FF",  "#1794CEFF","#972C8DFF"),
-    
+
     color_set_2 = c("#B15928", "#CAB2D6", "#FF7F00", "#FDBF6F", "#E31A1C", "#FB9A99", "#33A02C", "#B2DF8A", "#1F78B4", "#A6CEE3", "#1170aa", "#fc7d0b", "#76B7B2", "#B07AA1", "#E15759", "#59A14F", "#EDC948", "#FF9DA7", "#9C755F","#BAB0AC","#C84248"),
     color_set_3 = c("#2366C0FF", "#E9D738FF", "#B91226FF", "#A3DA4BFF", "#FF6435FF"),
     color_set_4 = c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666")
   )
-  
+
   # Define groups based on the inputs
   groups <- c(group1, group2, group3, group4)  # Ensure these are the correct variable names for your groups
   groups <- groups[!is.null(groups)]
-  
+
   # Define hardcoded_colors for the current groups
   hardcoded_colors <- all_hardcoded_colors[1:length(groups)]
   names(hardcoded_colors) <- groups
-  
+
   # Generate group colors
   group_colors <- lapply(groups, function(x) generate_colors(x, hardcoded_colors[[x]]))
-  
+
   # Generate annotation_col
   annotation_col = generate_annotation_col(groups)
-  
+
   # Generate annotation_colors
   ann_colors <- c(group_colors, if(type %in% c("taxonomy", "taxanomy")) list(Phylum = phylum_col) else list(Path = Path_col))
   names(ann_colors) <- c(groups, if(type %in% c("taxonomy", "taxanomy")) "Phylum" else "Path")
-  
-  
-  
+
+
+
   #===== data process
-  
+
   if(type == "taxonomy" | type == "taxanomy" ){
     matrix = as.matrix(matrix)
   }else if(type == "function"){
     matrix <- t(matrix)
   }
-  
+
   colSums(matrix)
-  
+
   bk <- c(0,0.5,1)
   print("p0")
-  
+
   tt<-try(ComplexHeatmap::pheatmap(matrix, annotation_col = annotation_col),T)
   if (class(tt) == "try-error"){
     matrix <- t(matrix)
@@ -297,8 +302,8 @@ print("Check the psIN")
     print("with annotation_row")
     p <- ComplexHeatmap::pheatmap(matrix,  fontsize =8,main = title,
                                   #scale= "row",
-                                  annotation_col = annotation_col, 
-                                  annotation_row = annotation_row, 
+                                  annotation_col = annotation_col,
+                                  annotation_row = annotation_row,
                                   show_rownames = show_rownames,
                                   show_colnames = show_colnames,
                                   cluster_rows = cluster_rows,
@@ -319,9 +324,9 @@ print("Check the psIN")
     }else{
       matrix.orderd <- matrix
     }
-    
+
     p <- ComplexHeatmap::pheatmap(matrix.orderd,  fontsize =8, main = title,
-                                  annotation_col = annotation_col, 
+                                  annotation_col = annotation_col,
                                   show_rownames = show_rownames,
                                   show_colnames = show_colnames,
                                   cluster_rows = cluster_rows,
@@ -329,24 +334,24 @@ print("Check the psIN")
                                   labels_row=taxaTab$Rank,
                                   cutree_rows = cutree_rows, cutree_cols = cutree_cols,
                                   annotation_colors = ann_colors)
-    
-    
+
+
 
       print("p3")
   }
 
 
   # logic for out file
-  pdf(sprintf("%s/pheatmap.%s.%s%s%spdf", out_pdf, 
-              project, 
-              ifelse(is.null(col_orders), "", paste("ordered", ".", sep = "")), 
-              ifelse(is.null(title), "", paste(title, ".", sep = "")), 
-              ifelse(is.null(name), "", paste(name, ".", sep = "")), 
+  pdf(sprintf("%s/pheatmap.%s.%s%s%spdf", out_pdf,
+              project,
+              ifelse(is.null(col_orders), "", paste("ordered", ".", sep = "")),
+              ifelse(is.null(title), "", paste(title, ".", sep = "")),
+              ifelse(is.null(name), "", paste(name, ".", sep = "")),
               format(Sys.Date(), "%y%m%d")), height = h, width = width)
-  
+
   print("p4")
   print(p)
-  
+
   dev.off()
 }
 
