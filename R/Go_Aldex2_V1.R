e77371a3e4db4b3e37ba506733fc81b376a77ad5
bbagy <bbagy81@gmail.com>
2024-03-12 14:10:08 -0400

    Gotools_1.0.4

--- R/Go_Aldex2_V1.R
+++ R/Go_Aldex2_V1.R
@@ -365,7 +365,7 @@ Go_Aldex2 <- function(psIN,  project,
         confounder_prefix <- ifelse(is.null(confounders), "", "with_confounder")
         name_prefix <- ifelse(is.null(name), "", paste0(name, ""))
 
-        filename <- sprintf("%s/aldex2.(%s.vs.%s).Sig%s.%s.%s.%s%s.csv",
+        filename <- sprintf("%s/aldex2.(%s.vs.%s).Sig%s.%s.%s.%s%s%s.csv",
                             out_DA, basline, smvar, num_significant, mvar, model, confounder_prefix, name_prefix, project)
         write.csv(merged_results, quote = F, col.names = NA, file = filename)
       }
