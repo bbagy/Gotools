e13f3c8c0937d2dd24afb4981b6e8e06efd039a0
bbagy <bbagy81@gmail.com>
2024-04-10 22:34:32 -0400

    Gotools_1.0.5

--- R/Go_expInfo.R
+++ R/Go_expInfo.R
@@ -33,7 +33,7 @@
 #' exp_info <- Go_expInfo(kit_number = 6, prep_number = 1, spikein_number = 2)
 #' print(exp_info)
 
-Go_expInfo <- function(kit_number = NA, prep_number = NA, spikein_number = NA) {
+Go_expInfo <- function(kit_number = NA, prep_number = NA, spikein_number = NA, authorName1=NA ,authorName2 = NULL) {
   # Check if all arguments are missing and print options if they are
   if (is.na(kit_number) && is.na(prep_number) && is.na(spikein_number)) {
     cat("Available kit numbers and their descriptions:\n",
@@ -52,13 +52,14 @@ Go_expInfo <- function(kit_number = NA, prep_number = NA, spikein_number = NA) {
         "1: / ZymoBIOMICSTM Spike-in Control II (Low Microbial Load) (D6321)\n",
         "2: No Spike-in Control\n\n",
         "Available Technicians and email address:\n",
+        "Heekuk Park : hp2523@cumc.columbia.edu\n",
         "Sofia Moscovitz : szm2110@cumc.columbia.edu\n",
         "Dalia Moallem : dhm2127@cumc.columbia.edu\n",
         "Dwayne Seeram : ds4057@cumc.columbia.edu\n",
         "Kristen Lewis : kl2954@cumc.columbia.edu\n")
     return(invisible())
   }
-  
+
   # Define the kit based on kit_number
   kit <- switch(as.character(kit_number),
                 "1" = "ZymoBIOMICS 96 MagBead DNA/RNA Kit (Catalog# D4300)",
@@ -69,20 +70,48 @@ Go_expInfo <- function(kit_number = NA, prep_number = NA, spikein_number = NA) {
                 "6" = "DNeasy PowerSoil Pro Kit (Catalog# 47016)",
                 "7" = "DNeasy DNA and Blood kit (Catalog# 69506)",
                 "Unknown kit")
-  
+
   # Define the prep based on prep_number
   prep <- switch(as.character(prep_number),
                  "1" = "Illumina V3V4",
                  "2" = "Illumina V1V2",
                  "3" = "Zymo Quick-16S/ V3V4 Plus NGS Library prep kit (#catalog D6420)",
                  "Unknown prep")
-  
+
   # Define the spikein based on spikein_number
   spikein <- switch(as.character(spikein_number),
                     "1" = "/ ZymoBIOMICSTM Spike-in Control II (Low Microbial Load) (D6321)",
                     "2" = NULL,  # Representing no spike-in control
                     "Unknown Spike-in")
-  
-  # Return a list containing the selected options
-  return(list(kit = kit, prep = prep, spikein = spikein))
+
+  # Define email addresses based on author names
+  getEmail <- function(name) {
+    switch(name,
+           "Heekuk Park" = "hp2523@cumc.columbia.edu",
+           "Sofia Moscovitz" = "szm2110@cumc.columbia.edu",
+           "Dalia Moallem" = "dhm2127@cumc.columbia.edu",
+           "Dwayne Seeram" = "ds4057@cumc.columbia.edu",
+           "Kristen Lewis" = "kl2954@cumc.columbia.edu",
+           NA)
+  }
+
+  authorEmail1 <- getEmail(authorName1)
+  authorEmail2 <- if (!is.null(authorName2)) {
+    getEmail(authorName2)
+  } else {
+    NULL
+  }
+
+  contact_email <- c(authorEmail1, authorEmail2)
+  contact_email <- contact_email[!is.na(contact_email) & !is.null(contact_email)]  # Remove NA and NULL values
+
+  # Return a list containing the selected options, including author names and their email addresses
+  return(list(
+    kit = kit,
+    prep = prep,
+    spikein = spikein,
+    authorName1 = authorName1,
+    authorName2 = authorName2,
+    contact_email = contact_email
+  ))
 }
