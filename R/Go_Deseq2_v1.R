58c13dab87a79b1f29531a3349e46fcb9b883956
bbagy <bbagy81@gmail.com>
2024-04-12 11:00:36 -0400

    Gotools_1.0.5

--- R/Go_Deseq2_v1.R
+++ R/Go_Deseq2_v1.R
@@ -37,24 +37,24 @@
 #'
 #' @export
 
-Go_Deseq2 <- function(psIN,  project, 
-                      taxanames=NULL, 
-                      cate.outs,  
-                      cate.conf=NULL, 
-                      cont.conf=NULL, 
+Go_Deseq2 <- function(psIN,  project,
+                      taxanames=NULL,
+                      cate.outs,
+                      cate.conf=NULL,
+                      cont.conf=NULL,
                       orders=NULL,
                       name=NULL){
 
   # out dir
-  out <- file.path(sprintf("%s_%s",project, format(Sys.Date(), "%y%m%d"))) 
+  out <- file.path(sprintf("%s_%s",project, format(Sys.Date(), "%y%m%d")))
   if(!file_test("-d", out)) dir.create(out)
-  out_path <- file.path(sprintf("%s_%s/table",project, format(Sys.Date(), "%y%m%d"))) 
+  out_path <- file.path(sprintf("%s_%s/table",project, format(Sys.Date(), "%y%m%d")))
   if(!file_test("-d", out_path)) dir.create(out_path)
-  out_DA <- file.path(sprintf("%s_%s/table/Deseq2",project, format(Sys.Date(), "%y%m%d"))) 
+  out_DA <- file.path(sprintf("%s_%s/table/Deseq2",project, format(Sys.Date(), "%y%m%d")))
   if(!file_test("-d", out_DA)) dir.create(out_DA)
-  
-  
-  
+
+
+
   # taxa aggregate
   if(!is.null(taxanames)){
     psIN <- aggregate_taxa(psIN, taxanames)
@@ -62,12 +62,12 @@ Go_Deseq2 <- function(psIN,  project,
     psIN <- psIN
   }
   mapping <- data.frame(sample_data(psIN))
-  
-  
+
+
   # get data tyep
   print("Check the data type")
   taxtab.col <- colnames(data.frame((tax_table(psIN))))
-  
+
   if (any(grepl("Species", taxtab.col))){
     taxaTab <- data.frame(tax_table(psIN)[,"Species"])
     type <- "taxonomy"
@@ -85,9 +85,9 @@ Go_Deseq2 <- function(psIN,  project,
     type <- "RNAseq"
     print(type)
   }
-  
-  
-  
+
+
+
   # start
   res <- {}
   for (mvar in cate.outs ) {
@@ -115,8 +115,8 @@ Go_Deseq2 <- function(psIN,  project,
     }
 
 
-    
-    
+
+
     # integer control
     if (class(mapping.sel.na.rem[,mvar]) == "character"){
       mapping.sel.na.rem[,mvar] <- factor(mapping.sel.na.rem[,mvar])
@@ -129,7 +129,7 @@ Go_Deseq2 <- function(psIN,  project,
 
 
 
-    # for changing "-" to character 
+    # for changing "-" to character
     mapping.sel[,mvar] <- gsub("V-","Vn",mapping.sel[,mvar])
 
     # combination
@@ -138,28 +138,28 @@ Go_Deseq2 <- function(psIN,  project,
     }else{
      mapping.sel[,mvar] <- factor(mapping.sel[,mvar])
     }
-    
+
     # mapping.sel[,mvar] <- factor(mapping.sel[,mvar])
     cbn <- combn(x = levels(mapping.sel[,mvar]), m = 2)
-    
+
     my_comparisons <- {}
     for(i in 1:ncol(cbn)){
       x <- cbn[,i]
       my_comparisons[[i]] <- x
     };my_comparisons
-    
+
     # subset sample by combination
     for(i in 1:length(my_comparisons)){
     print(my_comparisons[i])
     combination <- unlist(my_comparisons[i]);combination
     basline <- combination[1]
     smvar <- combination[2]
-    
+
     mapping.sel.cb <- subset(mapping.sel, mapping.sel[[mvar]] %in% c(basline, smvar)) # phyloseq subset은 작동을 안한다.
-    
+
     psIN.cb <- psIN.na
     sample_data(psIN.cb) <- mapping.sel.cb
-    
+
 
     #-- DESeq2 for phyloseq --#
     gm_mean = function(x, na.rm=TRUE){
@@ -182,16 +182,16 @@ Go_Deseq2 <- function(psIN,  project,
         sample_data(psIN.cb) <- mapping.sel.cb
       }
     }
-    
-    
-    
-    
+
+
+
+
     if (!is.null(cate.conf) | !is.null(cont.conf)) {
       confounder <- c(cate.conf,cont.conf)
-      
+
       form <-as.formula(sprintf("~ %s + %s", mvar, paste(setdiff(confounder, "SampleType"), collapse="+")))
       print(form)
-      
+
       dds = phyloseq:::phyloseq_to_deseq2(psIN.cb, form)
     }    else {
       confounder <- NULL
@@ -205,7 +205,7 @@ Go_Deseq2 <- function(psIN,  project,
     vst = getVarianceStabilizedData(dds)
     dds = DESeq(dds, fitType="local")
     resultsNames(dds)
- 
+
     # calculation
       print("pass2")
       tmp <- as.data.frame(results(dds, contrast = c(mvar, smvar, basline)))
@@ -213,69 +213,69 @@ Go_Deseq2 <- function(psIN,  project,
         tmp <- unlist(strsplit(x, ";"))
         tmp[length(tmp)]
       }))
-      
-      
+
+
       tmp$mvar <- mvar
       tmp$basline<-basline
       tmp$bas.count <-  sum(with(mapping.sel.cb, mapping.sel.cb[,mvar] == basline))
       tmp$smvar <- smvar
       tmp$smvar.count <-  sum(with(mapping.sel.cb, mapping.sel.cb[,mvar] == smvar))
 
-      
+
       #-- give taxa name --#
       # Extract the taxonomy table from the phyloseq object
       tax_table <- as.data.frame(tax_table(psIN.cb))
       res <- merge(tmp, tax_table, by = 0, all.x = TRUE)
       res[res == "NA NA"] <- NA
-      
+
       tt <- try(filled_data <- apply(res[, c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")], 1, function(x) na.locf(x, na.rm = FALSE)), T)
-      
-      
+
+
       if(inherits(tt, "try-error")){
         filled_data <- apply(res[, c("Rank1", "Phylum", "Class", "Order", "Family", "Genus", "Species")], 1, function(x) na.locf(x, na.rm = FALSE))
       }
-      
-      
-      
-      
-      
-      
-      
+
+
+
+
+
+
+
       filled_data <- t(as.data.frame(filled_data))
       tt <- try(res[, c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")] <- filled_data, T)
-      
+
       if(inherits(tt, "try-error")){
         res[, c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")] <- t(filled_data)
       }
-      
-      
+
+
 
       if (type == "taxonomy"){
         taxaRanks <- c("Kingdon","Phylum","Class","Order","Family","Genus","Species")
         for(t in 2:length(taxaRanks)){
-          
+
           if (!is.null(taxanames)) {
             if (taxanames == taxaRanks[t-1]){
               break
             }
           }
-          
+
           res[,taxaRanks[t]] == "NA"
           res[,taxaRanks[t]]<- as.character(res[,taxaRanks[t]])
           res[,taxaRanks[t]][is.na(res[,taxaRanks[t]])] <- "__"
-          
+
           mask <- res[,taxaRanks[t]] %in% c("s__", "g__", "f__", "o__", "c__", "p__", "__")
           res[mask, taxaRanks[t]] <- ""
         }
-        
-        
+
+
         print("pass4")
         res$TaxaName <- paste(res$Phylum,"",res$Class,"",res$Order,"",res$Family,"",res$Genus,"",res$Species)
-        
+
         res$Species[res$Species=="NA NA"] <- "  "
-        
-        
-        
+
+
+
         print("pass5")
         #-- create table --#
         res <- as.data.frame(res)
@@ -292,51 +292,51 @@ Go_Deseq2 <- function(psIN,  project,
         res$deseq2.FDR <- ifelse(res$padj < 0.05, ifelse(sign(res$log2FoldChange)==1, "up", "down"), "NS")
         res$deseq2.P <- ifelse(res$pvalue < 0.05, ifelse(sign(res$log2FoldChange)==1, "up", "down"), "NS")
       }
-      
-      
-      
-      # get ps objectonly significant taxa 
+
+
+
+      # get ps objectonly significant taxa
       #res.sel <- subset(res, res$ancom  == T & !(res$deseq2  == "NS"));dim(res.sel)[1]
       res.sel <- subset(res, res$deseq2.FDR  %in% c("up","down"));dim(res.sel)[1]
       #taxa_sig <- res.sel$Row.names[1:dim(res.sel)[1]]; summary(taxa_sig)
-      
+
       #if(dim(res.sel)[1] == 0){
       #  ps.taxa.sig <- psIN.cb
       #}else{
       #  ps.taxa.sig <- prune_taxa(taxa_sig, psIN.cb)
       #  print(ps.taxa.sig)
       #}
-      
+
       # "name definition
       if (class(name) == "function"){
         name <- NULL
       }
 
-      # for changing "n" to "-" 
+      # for changing "n" to "-"
       res$basline <- gsub("Vn","V-",res$basline)
       res$smvar <- gsub("Vn","V-",res$smvar)
 
       res <- arrange(res, res$padj)
 
-      
+
       # Construct the common part of the file name
       file_prefix <- sprintf("(%s.vs.%s).Sig%s.%s", basline, smvar, dim(res.sel)[1], mvar)
-      
+
       # Optional parts
       taxa_suffix <- ifelse(is.null(taxanames), "", paste0(taxanames, "."))
-      confounder_suffix <- ifelse(is.null(confounder), "", "with_confounder")
+      confounder_suffix <- ifelse(is.null(confounder), "", paste0("with_confounder", "."))
       name_suffix <- ifelse(is.null(name), "", paste0(name, "."))
-      
+
       # Complete file names
       csv_filename <- sprintf("%s/deseq2.%s.%s%s%s%s.csv", out_DA, file_prefix, taxa_suffix, confounder_suffix, name_suffix, project)
       # rds_filename <- sprintf("%s/deseq2.%s.%s%s%s%s.rds", out_DA.ps, file_prefix, taxa_suffix, confounder_suffix, name_suffix, project)
-      
+
       # Write the csv and rds files
       write.csv(res, quote = FALSE, col.names = NA, file = csv_filename)
       #saveRDS(ps.taxa.sig, rds_filename)
-      
-      
-      
+
+
+
     }
   }
 }
