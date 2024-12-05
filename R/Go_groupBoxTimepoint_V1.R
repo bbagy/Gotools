241a9dbc196e38376b7e138f0da3f14b4b9dee36
bbagy <bbagy81@gmail.com>
2024-12-05 10:53:47 -0500

    Gotools_1.0.8

--- R/Go_groupBoxTimepoint_V1.R
+++ R/Go_groupBoxTimepoint_V1.R
@@ -61,17 +61,17 @@ Go_groupBoxTimepoint <- function(df = NULL,
                                  x.axis = NULL,
                                  height = NULL,
                                  width = NULL) {
-  
+
   library(dplyr)
   library(ggplot2)
   library(ggpubr)
-  
+
   # 데이터 초기화 및 기본값 설정
   if (is.null(df)) stop("Input dataframe (df) is required.")
   if (is.null(project)) stop("Project name is required.")
-  
+
   df.sel <- df  # 입력 데이터 그대로 사용
-  
+
   # colors and orders 처리
   if (is.null(mycols)) {
     print("mycols is not defined. Default colors will be used.")
@@ -79,70 +79,70 @@ Go_groupBoxTimepoint <- function(df = NULL,
   if (is.null(orders)) {
     print("orders is not defined.")
   }
-  
+
   # height, width 기본값 설정
   if (is.null(height)) height <- 3.5
   if (is.null(width)) width <- 5
-  
+
   # 변수 반복 처리
   for (variable in outcomes) {
     if (!is.null(dev.list())) dev.off()
-    
+
     # 평균 데이터 계산
     average_data <- df.sel %>%
-      group_by(!!sym(timepoint), !!sym(maingroup)) %>%
-      summarise(
+      dplyr::group_by(!!sym(timepoint), !!sym(maingroup)) %>%
+      dplyr::summarise(
         !!paste0("mean_", variable) := mean(!!sym(variable), na.rm = TRUE), # 동적 요약 변수
         .groups = "drop"
       ) %>%
       ungroup()
-    
+
     # Orders 업데이트
     if (!is.null(orders)) {
       df.sel[, timepoint] <- factor(df.sel[, timepoint], levels = intersect(orders, df.sel[, timepoint]))
       df.sel[, maingroup] <- factor(df.sel[, maingroup], levels = intersect(orders, df.sel[, maingroup]))
     }
-    
+
     # max 값 안전 처리
     max_label_y <- if (all(is.na(df.sel[[variable]]))) 1 else max(df.sel[[variable]], na.rm = TRUE) + 0.2
-    
+
     # 플롯 생성
-    p <- ggplot(df.sel, aes_string(x = timepoint, y = variable, fill = maingroup)) +  
+    p <- ggplot(df.sel, aes_string(x = timepoint, y = variable, fill = maingroup)) +
       geom_boxplot(outlier.shape = NA, lwd = 0.3) +
       geom_line(data = average_data, aes_string(x = timepoint, y = sprintf("mean_%s", variable), group = maingroup, colour = maingroup), size = 1, alpha = 0.4) +
       geom_point(aes_string(colour = maingroup), position = position_dodge(width = 0.75), size = 1.5, alpha = 0.8) +
-      theme_bw() + 
-      theme(legend.position = "bottom", legend.title = element_blank()) + 
+      theme_bw() +
+      theme(legend.position = "bottom", legend.title = element_blank()) +
       labs(y = variable, x = NULL) +
-      stat_compare_means(method = "wilcox.test", label = "p.format", 
-                         aes_string(group = maingroup), 
+      stat_compare_means(method = "wilcox.test", label = "p.format",
+                         aes_string(group = maingroup),
                          label.y = max_label_y)
-    
+
     # 색상 적용
     if (!is.null(mycols)) {
-      p <- p + scale_fill_manual(values = mycols, labels = fill_labels) + 
+      p <- p + scale_fill_manual(values = mycols, labels = fill_labels) +
         scale_colour_manual(values = mycols, labels = fill_labels)
     }
-    
-    
+
+
     if (!is.null(title)) {
       p <- p + ggtitle(sprintf("%s (%s)", title,"wilcox.test"))
     } else{
       p <- p + ggtitle(sprintf("%s (%s)", maingroup, "wilcox.test"))
     }
-    
-    
-    
+
+
+
     # 출력 경로 설정 및 저장
     dir <- Go_path(project, pdf = "yes", table = "no", path = NULL)
-    
-    pdf(sprintf("%s/groupBox3.%s.%s.%s%s%s.pdf", dir$pdf, 
-                project, 
+
+    pdf(sprintf("%s/groupBox3.%s.%s.%s%s%s.pdf", dir$pdf,
+                project,
                 maingroup,
-                ifelse(is.null(variable), "", paste(variable, ".", sep = "")), 
-                ifelse(is.null(name), "", paste(name, ".", sep = "")), 
+                ifelse(is.null(variable), "", paste(variable, ".", sep = "")),
+                ifelse(is.null(name), "", paste(name, ".", sep = "")),
                 format(Sys.Date(), "%y%m%d")), height = height, width = width)
-    
+
     print(p)
     dev.off()
   }
